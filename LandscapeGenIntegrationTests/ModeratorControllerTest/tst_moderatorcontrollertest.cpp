0b23be57539c26d990565ea504cc7141bb065ca3
Inlucker <arseny707@mail.ru>
2023-01-09 19:10:35 +0300

    added moderatorcontrollertest

--- LandscapeGenIntegrationTests/ModeratorControllerTest/tst_moderatorcontrollertest.cpp
+++ LandscapeGenIntegrationTests/ModeratorControllerTest/tst_moderatorcontrollertest.cpp
@@ -27,7 +27,7 @@ private slots:
 
 private:
     unique_ptr<ModeratorController> moderator_controller;
-    unique_ptr<UsersRepository> user_repository;
+    unique_ptr<UsersRepository> users_repository;
     unique_ptr<QSqlDatabase> db;
     string m_schema;
 };
@@ -49,7 +49,7 @@ ModeratorControllerTest::ModeratorControllerTest()
     db = DataBaseBuilder::createPostgresDataBase(m_dbhost, m_dbport, m_dbname, m_dbuser, m_dbpass, m_schema);
 
     moderator_controller = make_unique<ModeratorController>();
-    user_repository = make_unique<UsersRepository>();
+    users_repository = make_unique<UsersRepository>();
 }
 
 ModeratorControllerTest::~ModeratorControllerTest()
@@ -59,32 +59,65 @@ ModeratorControllerTest::~ModeratorControllerTest()
 
 void ModeratorControllerTest::initTestCase()
 {
+    if (DataBaseBuilder::createSchema() != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
 
 }
 
 void ModeratorControllerTest::init()
 {
+    if (DataBaseBuilder::createUsersTable() != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
 
+    if (DataBaseBuilder::fillUsersTable() != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
+
+    shared_ptr<UserBL> ubl = users_repository->getUser("moderator1", "123");
+    moderator_controller->login(ubl);
 }
 
 void ModeratorControllerTest::addUserTest()
 {
+    //ARRANGE
+    shared_ptr<UserBL> canvas_user = users_repository->getCanvasUser("user1");
+    UserBL new_user_bl(canvas_user->getId(), canvas_user->getLogin(), canvas_user->getPassword(), canvas_user->getRole(), moderator_controller->getUser()->getId());
 
+    //ACT
+    users_repository->updateUser(new_user_bl, canvas_user->getId());
+
+    //ASSERT
+    shared_ptr<UserBL> user;
+    if (DataBaseBuilder::getUser(user, 1) != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
+    QVERIFY2(user->getModeratorId() == moderator_controller->getUser()->getId(), "Error in addUserTest");
 }
 
 void ModeratorControllerTest::deleteUserTest()
 {
+    //ARRANGE
+    shared_ptr<UserBL> canvas_user = users_repository->getCanvasUser("user3");
+    UserBL new_user_bl(canvas_user->getId(), canvas_user->getLogin(), canvas_user->getPassword(), canvas_user->getRole(), -1);
 
+    //ACT
+    users_repository->updateUser(new_user_bl, canvas_user->getId());
+
+    //ASSERT
+    shared_ptr<UserBL> user;
+    if (DataBaseBuilder::getUser(user, 4) != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
+    QVERIFY2(user->getModeratorId() == 0, "Error in deleteUserTest");
 }
 
 void ModeratorControllerTest::cleanup()
 {
-
+    if (DataBaseBuilder::dropUsersTable() != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
 }
 
 void ModeratorControllerTest::cleanupTestCase()
 {
-
+    if (DataBaseBuilder::dropSchemaCascade() != OK)
+        QFAIL(DataBaseBuilder::lastError().c_str());
 }
 
 QTEST_APPLESS_MAIN(ModeratorControllerTest)
